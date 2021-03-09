d887dfb8be21b04c309a7ca324152c9272d958fc
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2021-03-09 16:49:43 +0100

    First time

---
+++ src/main/java/RomanNumberConverter.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class RomanNumberConverter {
+     Integer[] dec = {1,4,5,9,10};
+     String[] Rom = {"I", "", "V", "", "X"};
+
+    public RomanNumberConverter() {
+    }
+
+    StringBuilder stringBuilder = new StringBuilder();
+    public String Convert(int number){
+       if (number == 4)
+
+    }
+}
---
+++ src/test/java/RomanNumeralExercise.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class RomanNumeralExercise {
+    @Test
+    public void oneToThree(){
+        RomanNumberConverter converter = new RomanNumberConverter();
+    }
+
+    @Test
+    public void fourToEight(){
+
+    }
+
+    @Test
+    public void nineToForty(){
+
+    }
+}
